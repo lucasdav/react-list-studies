afd142d1c9cc71fc094d5d32f742879a409df98c
lucasdav <lucassouzadavanso@gmail.com>
2024-02-15 23:33:02 -0300

    inclusao state em app

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -4,12 +4,16 @@ import style from './Botao.module.scss'
 
 interface BotaoProps {
     children: ReactNode;
+    type?: "button" | "submit" | "reset" | undefined
 }
 
 class Botao extends React.Component<BotaoProps> {
+    
     render() {
+        const { type = "button" } = this.props;
+
         return(
-            <button className={style.botao}>
+            <button type={type} className={style.botao}>
                 {this.props.children}
             </button>
         )

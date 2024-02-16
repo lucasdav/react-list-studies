abecbb0f31b6f73a5781786e1cab61dcaeeef0ff
lucasdav <lucassouzadavanso@gmail.com>
2024-02-16 17:43:26 -0300

    inclusao item selecionado

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -16,13 +16,16 @@ export default function Item(
     }: Readonly<Props>) {
     console.log('item atual: ', {tarefa, tempo, selecionado, completado, id})
     return(
-        <li className={style.item} onClick={() => selecionaTarefa({
-            tarefa, 
-            tempo, 
-            selecionado, 
-            completado, 
-            id
-        })}>
+        <li 
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`} 
+            onClick={() => selecionaTarefa({
+                tarefa, 
+                tempo, 
+                selecionado, 
+                completado, 
+                id
+            })}
+        >
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
         </li>

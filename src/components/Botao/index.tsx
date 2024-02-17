0f64a8b12c85a15dbe4822a1c8cea07d68be302c
lucasdav <lucassouzadavanso@gmail.com>
2024-02-17 11:18:48 -0300

    refatoracao botao e formulario component

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,25 +1,22 @@
 import React, { ReactNode } from 'react';
 import style from './Botao.module.scss'
 
-
-interface BotaoProps {
-    children: ReactNode,
+interface Props {
+    children?: ReactNode,
     type?: "button" | "submit" | "reset" | undefined,
     onClick?: () => void
 }
 
-class Botao extends React.Component<BotaoProps> {
-    
-    render() {
-        const { type = "button", onClick } = this.props;
-
-        return(
-            <button onClick={onClick} type={type} className={style.botao}>
-                {this.props.children}
-            </button>
-        )
-        
-    }
+function Botao({ onClick, type, children }: Props) {
+    return (
+        <button 
+            onClick={onClick} 
+            type={type} 
+            className={style.botao}>
+            {children}
+        </button>
+    )
 }
+
 
 export default Botao;
